0f359d20eb64ea7e4d03c8e3a0cddd0336f8de06
JohnDergneel <dergneeljohn@gmail.com>
2023-02-16 12:39:40 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - the code entry
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzl\n");
+return (0);
+}
