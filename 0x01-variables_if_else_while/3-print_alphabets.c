229d2d564e01d0f0f7c1da9858e9c9e1d971bbfc
JohnDergneel <dergneeljohn@gmail.com>
2023-02-18 00:59:26 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -9,14 +8,20 @@ int main(void)
 {
 char i;
 char a;
- 
-for (i =  'a';  i <=  'z';  i++);
+i = 'a';
+while
+(i <=  'z')
 {
-putchar(char *i);
+putchar(i);
+i++;
 }
-for (a =  'A';  a <=  'Z'; a++);
+a = 'A';
+while
+(a <=  'Z')
 {
-putchar(char *j);
+putchar(a);
+a++;
 }
+putchar('\n');
 return (0);
 }
