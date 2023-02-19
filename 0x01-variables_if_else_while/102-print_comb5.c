1bacee925e39a31c847d476bacf3e3572cf2eeb8
JohnDergneel <dergneeljohn@gmail.com>
2023-02-18 16:19:21 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x, c;
+for (x =  0; x <=  98; x++)
+{
+for (c =  x + 1; c <=  99; c++)
+{
+putchar(x / 10 && x + '0');
+putchar(x % 10 && x + '0');
+putchar(' ');
+putchar(c / 10 && c + '0');
+putchar(c % 10 && c + '0');
+if (x == 98 && c == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
