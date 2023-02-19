8fca63402e68315607a2044018bf2948e776094e
JohnDergneel <dergneeljohn@gmail.com>
2023-02-18 16:50:18 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,15 +13,18 @@ for (x =  0; x <=  98; x++)
 {
 for (c =  x + 1; c <=  99; c++)
 {
-putchar(x / 10 && x + '0');
-putchar(x % 10 && x + '0');
+if (c != x)
+{
+putchar((x / 10) + '0');
+putchar((x % 10) + '0');
 putchar(' ');
-putchar(c / 10 && c + '0');
-putchar(c % 10 && c + '0');
+putchar((c / 10) + '0');
+ putchar((c % 10) + '0');
 if (x == 98 && c == 99)
 continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
