fa3fcc974a39f68b09358f511c972c99b3e505e2
JohnDergneel <dergneeljohn@gmail.com>
2023-02-22 21:12:11 -0800

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,10 +7,10 @@ int main(void)
 {
 int x, sum = 0;
 for (x = 0; x < 1024; x++)
-if ((x % 5) == 0 || (x % 3) == 0)
-sum += 1;
 {
-printf("%d\n", sum);
+if ((x % 3) == 0 || (x % 5) == 0)
+sum += x;
 }
+printf("%d\n", sum);
 return (0);
 }
